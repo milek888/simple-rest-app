f6419f76c71394657042f7b41052c32bdb6c74bd
hhh <milosz.mazurek@gmail.com>
2020-10-15 14:17:55 +0200

    Corrected tests

--- src/test/java/org/milosz/simplerestapp/other/QuickSortTest.java
+++ src/test/java/org/milosz/simplerestapp/other/QuickSortTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class QuickSortTest {
     static Stream<Arguments> argumentProvider() {
-        return Stream.of(Arguments.arguments(new int[]{8,7,5,11,0,5}, new int[]{0,50,5,7,8,11}),
+        return Stream.of(Arguments.arguments(new int[]{8,7,5,11,0,5}, new int[]{0,5,5,7,8,11}),
                 Arguments.arguments(new int[]{5,3,2,6,4,1,3,7}, new int[]{1,2,3,3,4,5,6,7}));
     }
 
